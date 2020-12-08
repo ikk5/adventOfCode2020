90749033eb975bd35a9f0c8a8e8fc07f0d0728d3
Benjamin Hofstede <benjamin.hofstede@sogeti.com>
2020-12-08 19:19:30 +0100

    day 7 refactoring

--- src/main/java/Opdr7.java
+++ src/main/java/Opdr7.java
@@ -1,11 +1,11 @@
-import helperObjects.Bag;
-import utils.ReadFileUtil;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import helperObjects.Bag;
+import utils.ReadFileUtil;
 
 public class Opdr7 extends AbstractOpdr {
 
@@ -29,33 +29,38 @@ public class Opdr7 extends AbstractOpdr {
 
     private Map<String, Bag> buildBagMap(List<String> bagLines) {
         Map<String, Bag> bags = new HashMap<>();
-        // Maak eerst alle Bags
         String bagRegex = "(.*) bags contain (.*)\\.";
+        String bagContentRegex = "(\\d+) (.*) bags?";
+
         for (String bagLine : bagLines) {
             Matcher matcher = Pattern.compile(bagRegex).matcher(bagLine);
             if (matcher.matches()) {
                 String bagName = matcher.group(1);
-                Bag bag = new Bag(bagName);
-                bags.put(bag.getBagName(), bag);
-            }
-        }
-
-        // Voeg de inhoud toe.
-        String bagContentRegex = "(\\d+) (.*) bags?";
-        for (String bagLine : bagLines) {
-            Matcher matcher = Pattern.compile(bagRegex).matcher(bagLine);
-            if (matcher.matches()) {
-                Bag bag = bags.get(matcher.group(1));
-                String contents = matcher.group(2);
-                for (String bagContent : contents.split(", ")) {
-                    Matcher contentMatcher = Pattern.compile(bagContentRegex).matcher(bagContent);
-                    if (contentMatcher.matches()) {
-                        String bagName = contentMatcher.group(2);
-                        bag.addContent(Integer.parseInt(contentMatcher.group(1)), bags.get(bagName));
-                    }
-                }
+                Bag bag = getOrAddBag(bags, bagName);
+                addInnerBags(bags, bagContentRegex, bag, matcher.group(2));
             }
         }
         return bags;
+    }
+
+    private void addInnerBags(Map<String, Bag> bags, String bagContentRegex, Bag bag, String contents) {
+        for (String bagContent : contents.split(", ")) {
+            Matcher contentMatcher = Pattern.compile(bagContentRegex).matcher(bagContent);
+            if (contentMatcher.matches()) {
+                String innerBagName = contentMatcher.group(2);
+
+                Bag innerBag = getOrAddBag(bags, innerBagName);
+                bag.addContent(Integer.parseInt(contentMatcher.group(1)), innerBag);
+            }
+        }
+    }
+
+    private Bag getOrAddBag(Map<String, Bag> bags, String bagName) {
+        Bag bag = bags.get(bagName);
+        if (bag == null) {
+            bag = new Bag(bagName);
+            bags.put(bagName, bag);
+        }
+        return bag;
     }
 }
